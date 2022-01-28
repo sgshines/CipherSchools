4ed4aea25f4d25ffc857432dcd3259672ee5b7bc
Sagar Sharma <57283639+sgshines@users.noreply.github.com>
2022-01-28 21:17:36 +0530

    Add files via upload

---
+++ Assignment 1/main.js
@@ -0,0 +1,47 @@
+let timer = document.getElementById('stopwatch');
+var min = 00;
+var sec = 00;
+var stoptime = true;
+
+function startTimer() {
+  if (stoptime == true) {
+        stoptime = false;
+        timerCycle();
+    }
+}
+function stopTimer() {
+  if (stoptime == false) {
+    stoptime = true;
+  }
+}
+
+function timerCycle() {
+    if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+    sec = sec + 1;
+
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+
+    if (sec < 10 || sec == 0) {
+      sec = '0' + sec;
+    }
+    if (min < 10 || min == 0) {
+      min = '0' + min;
+    }
+
+    timer.innerHTML = min + ':' + sec;
+
+    setTimeout("timerCycle()", 1000);
+  }
+}
+
+function resetTimer() {
+    timer.innerHTML = '00:00';
+    stoptime = true;
+    sec = 0;
+    min = 0;
+}
